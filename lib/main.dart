72537398247940a89f44ff601147ae6fcf9117e9
Randinr <162928047+Randinr@users.noreply.github.com>
2024-05-24 19:22:40 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'input_page.dart';
 
 void main() {
@@ -9,15 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BMI Calculator',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 122, 59, 47)),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 246, 0, 41)),
         useMaterial3: true,
       ),
-      home: InputPage(title: 'CALCULATOR BMI'),
+      home: const UserInput(title: 'BMI Calculator'),
     );
   }
 }
