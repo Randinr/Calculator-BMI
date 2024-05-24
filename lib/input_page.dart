b017ba2a9e831d82440dedf31577b71aef5b5dd2
Randinr <162928047+Randinr@users.noreply.github.com>
2024-05-24 19:22:08 +0700

    Update input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,79 +1,70 @@
 import 'package:flutter/material.dart';
-
 import 'bmi_result.dart';
 
-class InputPage extends StatefulWidget {
-  const InputPage({super.key, required this.title});
-
+class UserInput extends StatefulWidget {
   final String title;
+  const UserInput({super.key, required this.title});
 
   @override
-  State<InputPage> createState() => _InputPageState();
+  State<UserInput> createState() => _UserInputState();
 }
 
-class _InputPageState extends State<InputPage> {
-
+class _UserInputState extends State<UserInput> {
   bool _isMale = true;
   double _height = 170.0;
-  double _weight = 91.0;
+  double _weight = 60.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        ),
-
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-            Text('Gender: ${_isMale ? 'Male': 'Female'}'
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              'Gender: ${_isMale ? 'Male' : 'Female'}',
             ),
-
-            ToggleButtons(children:[
+            const SizedBox(height: 16.0),
+            ToggleButtons(
+              isSelected: [_isMale, !_isMale],
+              onPressed: (index) => setState(() => _isMale = index == 0),
+              children: const [
                 Icon(Icons.male),
                 Icon(Icons.female),
               ],
-            isSelected: [_isMale, !_isMale],
-            onPressed: (index) => setState(() {
-              _isMale = index == 0;
-              }),
             ),
-
-            SizedBox(height: 16.0),
-            Text('height (cm): ${_height.toStringAsFixed(1)}'),
+            const SizedBox(height: 16.0),
+            Text('Height (cm): ${_height.toStringAsFixed(1)}'),
             Slider(
               value: _height,
-              min : 100.0,
-              max : 250.0,
+              min: 100.0,
+              max: 250.0,
               onChanged: (value) => setState(() => _height = value),
             ),
-
-            SizedBox(height: 16.0),
-            Text('weight (kg): ${_weight.toStringAsFixed(1)}'),
+            const SizedBox(height: 16.0),
+            Text('Weight (kg): ${_weight.toStringAsFixed(1)}'),
             Slider(
               value: _weight,
-              min : 30.0,
-              max : 150.0,
+              min: 30.0,
+              max: 150.0,
               onChanged: (value) => setState(() => _weight = value),
             ),
-
             const SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
+              onPressed: () => Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => BMIResult(
+                  builder: (context) => BmiResult(
                     isMale: _isMale,
                     height: _height,
                     weight: _weight,
                   ),
                 ),
-              );
-              },
-              child: Text('Calculate BMI'),
+              ),
+              child: const Text('Calculate BMI'),
             ),
           ],
         ),
